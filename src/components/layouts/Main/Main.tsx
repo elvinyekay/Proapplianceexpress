960bac73fe034cf839ba4b9171721e3e076235e1
ElvinYekayev <elvinyeka@gmail.com>
2025-02-10 03:19:37 +0400

    project folder structure created and added routing

--- src/components/layouts/Main/Main.tsx
+++ src/components/layouts/Main/Main.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router';
-import { Toaster } from 'sonner';
+// import { Toaster } from 'sonner';
 
 const Main = () => {
   return (
     <main>
       <h1>header</h1>
       <Outlet />
-      <Toaster position="top-center" richColors />
+      {/* <Toaster position="top-center" richColors /> */}
       <h1>footer</h1>
     </main>
   );
